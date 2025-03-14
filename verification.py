0baa4659a7dee39cf33e1ab007a36a1e9920bbff
ririlafleche <186311386+ririlafleche@users.noreply.github.com>
2025-03-14 10:29:22 +0100

    test de miller rabin

--- verification.py
+++ verification.py
@@ -1,7 +1,23 @@
-def verification(valeur):
-    if valeur%2 == 0:
-        return 0
-    else:
-        return 1
+def miller_rabin_test(n, base):
+    if n < 2:
+        return False
+    if n in (2, 3):
+        return True
+    if n % 2 == 0:
+        return False
     
-print("ceci est la branch2")
+    d, r = n - 1, 0
+    while d % 2 == 0:
+        d //= 2
+        r += 1
+    
+    x = pow(base, d, n)
+    if x == 1 or x == n - 1:
+        return True
+    
+    for _ in range(r - 1):
+        x = pow(x, 2, n)
+        if x == n - 1:
+            return True
+    
+    return False
