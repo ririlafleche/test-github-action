9ac05fe679c9402f9d6287c33da657929c921588
ririlafleche <186311386+ririlafleche@users.noreply.github.com>
2025-05-28 16:56:36 +0200

    modif malveillante test

--- mon_script.py
+++
@@ -1,9 +0,0 @@
-from miller_rabin_cyriac_benousaad.verification import miller_rabin_test
-lst_prime = []
-compteur = 0
-while compteur < 50:
-    for i in range(1000):
-        if miller_rabin_test(i,10):
-            lst_prime.append(i)
-            compteur += 1
-print(lst_prime)
